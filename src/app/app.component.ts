43e0b5a264859f2f00150e34da7a1e9547f818b6
Seema <seema.ideore@gmail.com>
2025-09-03 01:59:52 +0530

    project intro updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,81 @@ import { FooterComponent } from "./shared/footer/footer.component";
 import { HeaderComponent } from './shared/header/header.component';
 import { BannerComponent } from './customer/banner/banner.component';
 import { AuthService } from './services/auth.service';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,FooterComponent],
+  imports: [RouterOutlet, HeaderComponent,FooterComponent,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css'  
 })
 export class AppComponent implements OnInit{
   title = 'SmartCart';
-  constructor( private authService: AuthService, private router: Router ){}
+
+features: any = [
+{ heading : "✔ Free API Integration -",
+  data : "To implement and maintain  E-commerce flow"
+},
+{ heading:"✔ Token-based Authentication -",
+  data: "For verified Registration to Login via Authguard"
+},
+{ heading:"✔ Role-based Authorization - ",
+  data: "To control flow access Admin/ Customer/ User via RoleGuard"
+},
+{ heading:"✔ Declarative Control Flow - ", 
+  data: "Used @if, @for, @switch instead of legacy *ngIf / *ngFor"
+},
+{ heading:"✔ Standalone Components -" ,
+  data:"No NgModule needed, making code lightweight and modular."
+},
+{ heading:"✔ Deferrable Views (@defer) - ",
+  data: "Lazy-loaded login/register and product modules for faster initial load."
+},
+{ heading:"✔ Built-in Image Optimization -",
+  data:" Applied lazy loading (loading='lazy') and responsive images."
+},
+{ heading:"✔ Signals (Reactivity Model) - ",
+  data:"Simplified state management in cart and wishlist."
+},
+{ heading:"✔ Enhanced Router - ",
+  data:"Used loadComponent for lazy standalone component routing."
+},
+{ heading:"✔ Strong Typed Forms -", 
+  data:" Leveraged Angular 17 typed reactive forms for product add/update."
+},
+{ heading:"✔ Modern CSS Integration - ",
+  data: "Scoped styles and utility classes with Bootstrap integration."
+}]
+
+  visibleFeatures: any = [];
+  currentIndex = 0;
+  showIntro = true;
+
+  constructor( private authService: AuthService, private router: Router ){
+    this.showFeaturesOneByOne();
+  }
+showFeaturesOneByOne() {
+    const interval = setInterval(() => {
+      if (this.currentIndex < this.features.length) {
+        this.visibleFeatures.push(this.features[this.currentIndex]);
+        this.currentIndex++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 500);
+  }
+
+  startDemo() {
+    this.showIntro = false;
+    this.router.navigate(['']); // 👈 change to your landing page route
+    this.authService.logout();
+  }
+
+
   ngOnInit(): void {
-    this.router.navigate(['']);
+    // this.router.navigate(['']);
       this.authService.logout();
   }
 }
